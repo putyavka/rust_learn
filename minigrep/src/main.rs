babc5d3d71ae248f533bb08ba16d306c5eba5a76
putyavka-ubuntu <putyavka@gmail.com>
2018-05-13 12:55:48 +0300

    minigrep finished

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,16 +6,14 @@ use minigrep::Config;
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
+
 	let config = Config::new(&args).unwrap_or_else(|err| {
-		println!("Error parsing arguments: {}", err);
+		eprintln!("Error parsing arguments: {}", err);
 		process::exit(1);
 	});
 	
-	println!("query: '{}'", config.query);
-	println!("In filename: '{}'", config.filename);
-
 	if let Err(e) = minigrep::run(config) {
-		println!("Application error: {}", e);
+		eprintln!("Application error: {}", e);
 		process::exit(1);
 	}
 }
