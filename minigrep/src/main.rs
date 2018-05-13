c9e01e74e8c73c7e253d8cdbf5aff3034a01a239
putyavka-ubuntu <putyavka@gmail.com>
2018-05-13 12:49:20 +0300

    minigrep implemented

---
+++ minigrep/src/main.rs
@@ -0,0 +1,21 @@
+extern crate minigrep;
+
+use std::env;
+use std::process;
+use minigrep::Config;
+
+fn main() {
+	let args: Vec<String> = env::args().collect();
+	let config = Config::new(&args).unwrap_or_else(|err| {
+		println!("Error parsing arguments: {}", err);
+		process::exit(1);
+	});
+	
+	println!("query: '{}'", config.query);
+	println!("In filename: '{}'", config.filename);
+
+	if let Err(e) = minigrep::run(config) {
+		println!("Application error: {}", e);
+		process::exit(1);
+	}
+}
